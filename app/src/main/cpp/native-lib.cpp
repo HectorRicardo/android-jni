537d3a0f33dac37b015697ae2ea2a172ef5739ec
Hector Ricardo <hectorricardomendez@gmail.com>
2022-02-10 22:06:12 -0600

    Adding threads

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
 #include <string>
+#include <thread>
+#include "print.hpp"
 
 void callMethod(JNIEnv* env, jobject mainActivity) {
     jclass mainActivityClass = env->GetObjectClass(mainActivity);
@@ -8,10 +10,18 @@ void callMethod(JNIEnv* env, jobject mainActivity) {
     env->CallVoidMethod(mainActivity, j_method);
 }
 
+void threadBody() {
+    print("HECMEN here");
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_remember_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject mainActivity) {
     callMethod(env, mainActivity);
+
+    std::thread thr(threadBody);
+    thr.detach();
+
     return env->NewStringUTF("Hello from C++");
 }
