7732b39fcf4c35122d1893db0113452d04275edd
Hector Ricardo <hectorricardomendez@gmail.com>
2022-02-10 22:00:26 -0600

    Calling method

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,9 +1,17 @@
 #include <jni.h>
 #include <string>
 
+void callMethod(JNIEnv* env, jobject mainActivity) {
+    jclass mainActivityClass = env->GetObjectClass(mainActivity);
+    jmethodID
+        j_method = env->GetMethodID(mainActivityClass, "onThreadStart", "()V");
+    env->CallVoidMethod(mainActivity, j_method);
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_remember_MainActivity_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
+        jobject mainActivity) {
+    callMethod(env, mainActivity);
     return env->NewStringUTF("Hello from C++");
 }
