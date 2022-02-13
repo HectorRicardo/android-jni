7b757db91d11eb2efd1cf8234ea31031586b2eaf
Hector Ricardo <hectorricardomendez@gmail.com>
2022-02-13 10:24:02 -0600

    Reformatting

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,33 +3,33 @@
 #include <thread>
 #include "print.hpp"
 
-void callMethod(JNIEnv* env, jobject mainActivity) {
-    jclass mainActivityClass = env->GetObjectClass(mainActivity);
-    jmethodID
-        j_method = env->GetMethodID(mainActivityClass, "onThreadStart", "()V");
-    env->CallVoidMethod(mainActivity, j_method);
+void callMethod(JNIEnv *env, jobject mainActivity) {
+  jclass mainActivityClass = env->GetObjectClass(mainActivity);
+  jmethodID
+      j_method = env->GetMethodID(mainActivityClass, "onThreadStart", "()V");
+  env->CallVoidMethod(mainActivity, j_method);
 }
 
 void threadBody(JavaVM *javaVM, jobject mainActivityGlobal) {
-    JNIEnv *env;
-    javaVM->AttachCurrentThread(&env, nullptr);
-    callMethod(env, mainActivityGlobal);
-    env->DeleteGlobalRef(mainActivityGlobal);
-    javaVM->DetachCurrentThread();
+  JNIEnv *env;
+  javaVM->AttachCurrentThread(&env, nullptr);
+  callMethod(env, mainActivityGlobal);
+  env->DeleteGlobalRef(mainActivityGlobal);
+  javaVM->DetachCurrentThread();
 }
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_remember_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject mainActivity) {
-    callMethod(env, mainActivity);
+    JNIEnv *env,
+    jobject mainActivity) {
+  callMethod(env, mainActivity);
 
-    JavaVM *javaVM;
-    env->GetJavaVM(&javaVM);
+  JavaVM *javaVM;
+  env->GetJavaVM(&javaVM);
 
-    jobject mainActivityGlobal = env->NewGlobalRef(mainActivity);
-    std::thread thr(threadBody, javaVM, mainActivityGlobal);
-    thr.detach();
+  jobject mainActivityGlobal = env->NewGlobalRef(mainActivity);
+  std::thread thr(threadBody, javaVM, mainActivityGlobal);
+  thr.detach();
 
-    return env->NewStringUTF("Hello from C++");
+  return env->NewStringUTF("Hello from C++");
 }
